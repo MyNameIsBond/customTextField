d59ab41f295d4e445ead031ad8b356a5153b2cc2
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-11-26 12:16:45 +0000

    Initial Commit

---
+++ textField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  textField
+//
+//  Created by Tony Hajdini on 26/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ textField/textFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  textFieldApp.swift
+//  textField
+//
+//  Created by Tony Hajdini on 26/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct textFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
